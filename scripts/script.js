9aa91f4b6248055be064e7b2c20ca7317a2f9409
wxtiago <tiagobatista.tec@gmail.com>
2022-01-11 14:54:04 -0300

    Parte 6

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,7 @@ let lockBoard = false;
 
 function flipCard() {
     if (lockBoard) return;
+    if (this === firstCard) return;
 
     this.classList.add('flip');
     if (!hasFlippedCard) {
@@ -30,13 +31,24 @@ function checkForMath() {
 function disableCards() {
     firstCard.removeEventListener('click', flipcard);
     secondCard.removeEventListener('click', flipcard);
+
+    resetBoard();
 }
 
 function unflipCards() {
+    lockBoard = true;
+
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+
+        resetBoard();
     }, 1500);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null]
 }
 
 cards.forEach((card) => {
